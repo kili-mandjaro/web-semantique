2eec9623667ce3250b608ed2067ac736212f9f4e
unknown <mgaillard@IF501-210-11.insa-lyon.fr>
2015-10-20 09:53:29 +0200

    Commit initial.

---
+++ js/app-controller.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/* Controllers */
+
+var webSemantiqueControllers = angular.module('webSemantiqueControllers', []);
+
+webSemantiqueControllers.controller('SearchController', ['$scope',
+  function($scope) {
+      
+  }]);
---
+++ js/app.js
@@ -0,0 +1,7 @@
+'use strict';
+
+/* App Module */
+
+var webSemantiqueApp = angular.module('webSemantiqueApp', [
+  'webSemantiqueControllers'
+]);
