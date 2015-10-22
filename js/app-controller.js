be0de2023b4701391aa23cc5c07b784b585d7756
Brindesable <ollivier.kilian@gmail.com>
2015-10-22 02:24:45 +0000

    Ajout du module pour rechercher les mot cles

--- js/app-controller.js
+++ js/app-controller.js
@@ -3,15 +3,30 @@
 /* Controllers */
 
 var webSemantiqueControllers = angular.module('webSemantiqueControllers', [
-    'webSemantiqueUrlServices'
+    'webSemantiqueUrlServices',
+    'webSemantiqueKeywordsUriFinderServices'
 ]);
 
 webSemantiqueControllers.controller('SearchController', ['$scope', 'Recherche',
-  function($scope, Recherche) {
-      
-    $scope.LancerRecherche = function() {
-        Recherche.get({requete:$scope.recherche}, function(jsonData) {
-            console.log(jsonData);
-        });
-    };
-  }]);
+    function ($scope, Recherche) {
+
+        $scope.LancerRecherche = function () {
+            Recherche.get({requete: $scope.recherche}, function (jsonData) {
+                console.log(jsonData);
+            });
+        };
+    }]);
+
+webSemantiqueControllers.controller('AnalyseController', ['$scope', 'Analyse',
+    function ($scope, Analyse) {
+
+        $scope.keywords = [];
+        $scope.confidence = "0.2";
+        $scope.text = "Harry Potter is an English series of seven fantasy novels written by British author J. K. Rowling.";
+
+        $scope.LancerAnalyse = function () {
+            Analyse.getKeywordsURI($scope.text, $scope.confidence, function(keywords){
+                $scope.keywords = keywords;
+            })
+        };
+    }]);
